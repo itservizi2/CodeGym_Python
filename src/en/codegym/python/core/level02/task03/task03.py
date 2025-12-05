5fe7283a7fb785298ad20ca3112b4839dbbe3b73
itservizi2 <itservizi.helpdesk@gmail.com>
2025-12-05 15:44:59 +0200

    Python Level 3

---
+++ src/en/codegym/python/core/level02/task03/task03.py
@@ -0,0 +1,10 @@
+# Height in inches
+
+# Create a variable named centimeter and assign it your height in centimeters.
+# Create a variable named inches and assign it your height in inches (1 inch == 2.54 cm).
+# Print your height in inches to the screen.
+
+centimeter = float(input("Insert your height in centimeters: "))
+# 1 inch == 2.54 cm
+inches = centimeter / 2.54
+print("Your height in inches is ", inches)
